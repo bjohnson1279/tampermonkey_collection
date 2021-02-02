2af5a094635a05fd47ed1b915d45edc45b0417ed
bjohnson1279 <johnsonbrentp@gmail.com>
2021-02-02 13:34:39 -0700

    Added script to hide comments from certain KSL users

---
+++ kslCommentsHide.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         KSL Comments Blacklist
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Filter comments by obnoxious KSL users
+// @author       Brent Johnson
+// @match        https://www.ksl.com/article/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const container = document.querySelector('#commentsContainer');
+    const config = { attributes: true, childList: true, subtree: true };
+
+    const cbk = (ml, obs) => {
+        const cList = container.querySelector('.CommentsList__root');
+        if (cList) {
+            const allComments = cList.querySelectorAll('.CommentsList__item');
+            allComments.forEach(comment => {
+                const username = comment.querySelector('.CommentsList__userName').innerText;
+                // Add usernames to the array below to block
+                if ([].includes(username)) {
+                    console.log(`User ${username} comment removed`);
+                    comment.style.display = 'none';
+                }
+            });
+        }
+    };
+
+    const obs = new MutationObserver(cbk);
+    obs.observe(container, config);
+})();
